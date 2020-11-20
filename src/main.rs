36faf29826fcd4654f15577e30467dcce6f8340d
Serhii Cheredko <serhii.cheredko@crello.com>
2020-11-20 18:11:18 +0200

    Clean up the code

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,3 @@ extern crate regex;
 fn main() {
     cli::init();
 }
-
-// WAS DONE:
-// 1. Improved "simple" theme
-// 2. Added "dark" theme (rename it)
-// 3. Some fixes in code
-// TODO:
-// 1. Finish/improve "dark" theme: make good to print
-// 2. Improve README by adding list of available themes(and screenshots)
-// 3. Write message when script finishes writing HTML file
